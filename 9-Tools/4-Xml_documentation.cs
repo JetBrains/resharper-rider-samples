d65e6c2a4fc1a7294da2b6108d451446a10f707d
Matt Ellis <m.t.ellis@gmail.com>
2013-05-31 17:29:56 +0100

    Stack Trace Explorer

---
+++ 9-Tools/4-Xml_documentation.cs
@@ -0,0 +1,7 @@
+﻿namespace JetBrains.ReSharper.Koans.UnitTesting
+{
+    public class Xml_documentation
+    {
+         
+    }
+}
---
+++ 9-Tools/5-Clipboard_ring.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JetBrains.ReSharper.Koans.UnitTesting
+{
+    // Clipboard Ring
+    //
+    // Tracks the last 20 copies to the clipboard
+    //
+    // (No shortcut defined in VS)
+    // Ctrl+Shift+V (IntelliJ)
+
+    public class ClipboardRing
+    {
+    }
+}
