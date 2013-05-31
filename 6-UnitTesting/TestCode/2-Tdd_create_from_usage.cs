134ed15d143b4d5a4278b6788c2203fdc03a886b
Matt Ellis <m.t.ellis@gmail.com>
2013-05-31 17:00:32 +0100

    Minor rearranging

--- 6-UnitTesting/TestCode/2-Tdd_create_from_usage.cs
+++ 6-UnitTesting/TestCode/2-Tdd_create_from_usage.cs
@@ -12,8 +12,7 @@ namespace JetBrains.ReSharper.Koans.UnitTesting
             // 2. Alt+Enter on Calculator and create from usage
             // 3. Alt+Enter on Add and create from usage
             // 4. Implement Add to make test pass
-            // 5. Create a new project to hold production code
-            //    Refactor the Calculator class and move to the new project
+            // 5. Use the move refactoring to move the Calculator class to the UnitTesting-ProductionCode project
             //var calculator = new Calculator();
             //Assert.AreEqual(3, calculator.Add(1, 2));
         }
