8cf0ce8cc860eca2887909ca698c8761a2ba7508
Matt Ellis <m.t.ellis@gmail.com>
2013-05-31 15:32:00 +0100

    File structure window

---
+++ 1-Navigation/7-File_structure_window/7.1-File_structure_window.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace JetBrains.ReSharper.Koans.Navigation
+{
+    // File Structure Window
+    //
+    // Displays outline of file
+    //
+    // Ctrl+Alt+F (VS)
+    // Ctrl+F11 (IntelliJ)
+    //
+    // 1. Double click to navigate
+    // 2. Select "Track caret in editor" and move around the editor
+    // 3. Use the Next and Previous buttons to navigate around
+    // 4. Explore the right click options
+
+    public class FileStructureWindow : IComponent
+    {
+        public FileStructureWindow()
+        {
+            Name = "Hello";
+        }
+
+        public string Name { get; private set; }
+
+        // 1. In the file window, click the "x" in the box representing the region
+        //    this removes the region
+        #region Implementation of IComponent
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISite Site { get; set; }
+        public event EventHandler Disposed;
+
+        #endregion
+    }
+}
