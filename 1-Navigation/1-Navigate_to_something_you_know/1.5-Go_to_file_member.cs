67db551e6ec45cb2f52242b277acdf7b1e076e91
Matt Ellis <m.t.ellis@gmail.com>
2013-05-31 11:27:34 +0100

    Navigate to something you know

---
+++ 1-Navigation/1-Navigate_to_something_you_know/1.5-Go_to_file_member.cs
@@ -0,0 +1,40 @@
+﻿namespace JetBrains.ReSharper.Koans.Navigation
+{
+    // Go to File Member
+    //
+    // Navigate to any members defined in the current file
+    //
+    // Alt+\ (VS)
+    // Ctrl+F12 (IntelliJ)
+    //
+    // 1. Navigate to type by name, CamelHumps, middle matching or wildcard
+    //    Type "GoToFileMemberExample"
+    //    Type "GTFME"
+    //    Type "FileMember"
+    //    Type "GoTo*Example"
+    //
+    // 2. Navigate to class member by name
+    //    Type "foo" to navigate to GoToFileMemberExample.Foo
+    //
+    // 3. Navigate to constructor
+    //    Type "new" or "ctor"
+    //
+    // 4. Navigate to members by visibility
+    //    Type "public" or "private"
+
+
+    // ----------
+
+    public class GoToFileMemberExample
+    {
+        private readonly int bar;
+
+        public GoToFileMemberExample(string foo, int bar)
+        {
+            this.bar = bar;
+            Foo = foo;
+        }
+
+        public string Foo { get; private set; }
+    }
+}
---
+++ 1-Navigation/1-Navigate_to_something_you_know/ExampleCode/ExampleCode.cs
@@ -0,0 +1,30 @@
+﻿namespace JetBrains.ReSharper.Koans.Navigation.ExampleCode
+{
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    public class DocumentViewModel
+    {
+        // ...
+    }
+
+    public class SettingsViewModel
+    {
+        // ...
+    }
+
+    public class SettingsViewManager
+    {
+        // ...
+    }
+
+}
