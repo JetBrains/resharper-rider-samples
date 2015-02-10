b86b2651a86a537916af66dabd183e563364e93e
Matt Ellis <m.t.ellis@gmail.com>
2015-02-10 12:03:33 +0000

    Misc tweaks

--- 1-Navigation/2-Navigate_to_definition/2.1-Go_to_definition.cs
+++ 1-Navigation/2-Navigate_to_definition/2.1-Go_to_definition.cs
@@ -19,10 +19,10 @@ namespace JetBrains.ReSharper.Koans.Navigation
             // 2. Use the keyboard shortcut
             var person = new Person("Hadi", 53);
 
-            // 3. Go To Definition for "name" or "age" variable
+            // 3. Go To Definition for "name" or "age" parameters
             var otherPerson = new Person(name, age);
 
-            // 4. Go To Definition on the word "var"
+            // 4. Go To Definition on the word "var" to navigate to inferred type
             var thirdPerson = otherPerson;
 
             // 5. Navigate to definition of a type from a library
