4539abd7990758e11c88206b1e0c41e916d6d1d7
Matt Ellis <m.t.ellis@gmail.com>
2015-02-10 14:42:05 +0000

    Essential shortcuts

---
+++ 0-EssentialShortcuts/0.1-AltEnter.cs
@@ -0,0 +1,48 @@
+﻿namespace EssentialShortcuts
+{
+
+    // Alt+Enter
+    //
+    // Used to apply quick fixes to inspections ("squigglies"), or apply
+    // context specific actions, depending on location of text caret
+    //
+    // Also allows searching and applying all ReSharper commands direct
+    // from the menu.
+    //
+    // Icon is displayed in gutter margin on left of editor, e.g. yellow
+    // lightbulb to fix warning, red light bulb to fix error, hammer to
+    // apply a context action, etc.
+
+    // 1. Apply QuickFix for an inspection
+    //    The class doesn't match ReSharper's naming style
+    //    Place text caret on "squiggly". Note the lightbulb in the margin on the left
+    //    Click the light bulb, or hit "Alt+Enter"
+    //    Select "Rename to 'BadlyNamedClass'" to fix
+    //
+    // (More on inspections in section 3)
+    public class badlyNamedClass
+    {
+    }
+
+    public class ContextAction
+    {
+        public static string FormatString(string arg)
+        {
+            // 2. Apply context action
+            //    Place text caret on "arg"
+            //    Note the hammer action - a context action is available (no squiggly!)
+            //    Hit Alt+Enter, select "To String.Format invocation"
+            return "Hello" + arg + "World";
+        }
+    }
+
+    // 3. Go to action
+    //    Place text caret on class name below
+    //    Alt+Enter, note magnifying glass
+    //    Click magnifying glass, start typing "rename"
+    //    Or, just start typing "rename" from menu
+    //    Filters ReSharper commands and applies
+    public class GoToAction
+    {
+    }
+}
---
+++ 0-EssentialShortcuts/0.3-ContextualMenus.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialShortcuts
+{
+    // Navigate To menu
+    //
+    // Displays a contextual menu of options you can use to navigate to from
+    // your current location
+    //
+    // Very useful way of navigating without having to learn ALL of the shortcuts!
+    //
+    // Alt+` (VS)
+    // Ctrl+Shift+G (IntelliJ)
+    //
+    // 1. Place text caret on class name, invoke menu
+    // 2. Alt+Enter on class name, start typing (e.g. "Navigate To", "Extension methods")
+    //
+    // (Most options not useful with this example. Covered in more detail in next section)
+    public class NavigateTo
+    {
+    }
+
+
+    // Refactor This menu
+    //
+    // Displays a contextual menu of options for refactoring available at the current location
+    //
+    // Very useful for invoking refactorings without requiring to know ALL shortcuts
+    //
+    // Ctrl+Shift+R (VS + IntelliJ)
+    //
+    // 3. Place text caret on class name, invoke menu
+    //    Select rename, rename class
+    // 4. Alt+Enter on class name, start typing (e.g. "Refactor This", "Rename")
+    //
+    // Covered in more detail in next section
+    public class RefactorThis
+    {
+    }
+
+
+    // Inspect This menu
+    //
+    // Displays a contextual menu of options for analysing code and control flow
+    // at the current location
+    //
+    // Very useful for invoking value analysis without learning ALL shortcuts!
+    //
+    // Ctrl+Shift+Alt+A (VS + IntelliJ)
+    //
+    // 4. Place text caret on class name, invoke menu
+    // 5. Alt+Enter on class name, start typing (e.g. "Inspect This", "Hierarchies")
+    //
+    // (Most options not useful with this example. Covered in more detail in later section)
+    public class InspectThis
+    {
+    }
+}
