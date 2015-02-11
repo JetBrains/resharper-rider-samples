f03ab6dc6282875f7126d4893844baf8193e22d8
Matt Ellis <m.t.ellis@gmail.com>
2015-02-11 00:32:07 +0000

    Duplicate and comment code

---
+++ 02-Editing/6-Duplicating_code.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JetBrains.ReSharper.Koans.Editing
+{
+    // Duplicating code
+    //
+    // Ctrl+D (in text editor)
+
+    // 1. Duplicate single line
+    //    Place caret on line below, select Ctrl+D
+    using System.Collections;
+
+    // 2. Duplicating multiple lines
+    //    Select entire class (Use expand selection!)
+    //    Hit Ctrl+D to copy entire class
+    public class SimpleClass
+    {
+    }
+}
---
+++ 02-Editing/7-Commenting_code.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace JetBrains.ReSharper.Koans.Editing
+{
+    // Toggle line comment
+    //
+    // Ctrl+Alt+/ (VS)
+    // Ctrl+/ (IntelliJ)
+
+    // Toggle block comment
+    //
+    // Ctrl+Shift+/ (VS + IntelliJ)
+
+    // 1. Toggle comment on single line
+    //    Place caret on line below, select Ctrl+D
+    using System.Collections;
+
+    // 2. Toggle comment on multiple lines
+    //    Select both lines, invoke Toggle line comment
+    //    Toggle line comment to uncomment again
+    using System.Collections;
+    using System.Data;
+
+    // 3. Toggle block comment
+    //    Inserts block comment at text caret location
+    //    Toggle removes it again
+    using System.Collections;
+
+    // 4. Comment selection with block comment
+    //    Select some text, invoke block comment
+    //    Only creates block comment, doesn't remove
+    using System.Collections;
+}
