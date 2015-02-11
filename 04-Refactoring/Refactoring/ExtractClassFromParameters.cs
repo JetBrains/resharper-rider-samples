58ea5b73720521f32e0320dbf4cb4a7e282191fe
Matt Ellis <m.t.ellis@gmail.com>
2015-02-11 16:28:28 +0000

    Extract class from parameters

---
+++ 04-Refactoring/Refactoring/ExtractClassFromParameters.cs
@@ -0,0 +1,27 @@
+﻿namespace JetBrains.ReSharper.Koans.Refactoring
+{
+    // Extract Class From Parameters
+    //
+    // Creates a class based on the parameters to a method, and creates it at call sites
+    //
+    // No keyboard shortcut. Invoke via Refactor This menu
+    // Ctrl+Shift+R
+
+    public class ExtractClassFromParameters
+    {
+        // 1. Extract class from parameters
+        //    Place text caret on method definition
+        //    Invoke Refactor This → Extract Class From Parameters
+        //    ReSharper prompts for options - name, class/struct, nested/top level
+        //      and which parameters should be extracted
+        public ExtractClassFromParameters(string forename, string surname, int age)
+        {
+        }
+
+        public static ExtractClassFromParameters Create(string forename, string surname, int age)
+        {
+            // 2. Confirm call site has been updated to create parameter class
+            return new ExtractClassFromParameters(forename, surname, age);
+        }
+    }
+}
