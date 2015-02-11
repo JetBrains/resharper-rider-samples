664cc27bb54c3df7b1aa8833d9f01ed73c1e72cf
Matt Ellis <m.t.ellis@gmail.com>
2015-02-11 15:01:46 +0000

    Move refactoring

--- 04-Refactoring/Refactoring/04-Move.cs
+++ 04-Refactoring/Refactoring/04-Move.cs
@@ -20,7 +20,7 @@
         }
     }
 
-    // 2. Move to another folder
+    // 2. Move to another folder in the project
     //    Place text caret on type definition
     //    Invoke Move refactoring
     //    Select Move to another folder
@@ -31,6 +31,25 @@
     //    Click "Create this folder"
     //    Accept move, check type is moved to new location
     public class MoveToAnotherFolderInProject
+    {
+        public void Method()
+        {
+        }
+    }
+
+    // 3. Move to another folder in another project
+    //    Place text caret on type definition
+    //    Invoke Move refactoring
+    //    Select Move to another folder
+    //    ReSharper suggests classes to move and folder location
+    //    Start typing new location - ReSharper provides suggestions of existing folders and projects
+    //    Add "-MoveTarget" to end of current location
+    //      (Target folder should be: "04 Refactoring\Refactoring-MoveTarget")
+    //    Accept move, check type is moved to the new project
+    //    ReSharper detects there is no reference between this project and Refactoring-MoveTarget
+    //    Can Cancel the refactoring and add the reference
+    //    Or continue, and fix up the reference by Alt+Enter on the broken usage below
+    public class MoveToAnotherFolderInOtherProject
     {
         public void Method()
         {
@@ -59,6 +78,7 @@
             // 5. Ensure all types are still referenced correctly
             new MoveToAnotherFile().Method();
             new MoveToAnotherFolderInProject().Method();
+            new MoveToAnotherFolderInOtherProject().Method();
             new MoveToAnotherNamespace().Method();
         }
     }
