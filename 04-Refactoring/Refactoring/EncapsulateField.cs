cb940735b37e5f75ef5a2743e02541cff4ab126a
Matt Ellis <m.t.ellis@gmail.com>
2015-02-11 15:04:21 +0000

    Encapsulate Field

---
+++ 04-Refactoring/Refactoring/EncapsulateField.cs
@@ -0,0 +1,17 @@
+﻿namespace JetBrains.ReSharper.Koans.Refactoring
+{
+    // Encapsulate Field
+    //
+    // Convert a field to a property
+    //
+    // Ctrl+R, E
+
+    public class EncapsulateField
+    {
+        // 1. Encapsulate Field
+        //    Place text caret on field definition
+        //    Invoke Encapsulate Field
+        //    ReSharper prompts for options - auto property, read/write, visibility, etc.
+        public string Name;
+    }
+}
