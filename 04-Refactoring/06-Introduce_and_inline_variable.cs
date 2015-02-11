b027bb4be268405f503548f88eda85d630d9baab
Matt Ellis <m.t.ellis@gmail.com>
2015-02-11 12:29:08 +0000

    Introduce and inline variable

---
+++ 04-Refactoring/06-Introduce_and_inline_variable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace JetBrains.ReSharper.Koans.Refactoring
+{
+    // Introduce Variable
+    //
+    // Create a variable from selection
+    //
+    // Ctrl+R, V (VS)
+    // Ctrl+Alt+V (IntelliJ)
+
+    // Inline variable
+    //
+    // Replaces variable usage with the contents of the variable
+    //
+    // Ctrl+R, I (VS)
+    // Ctrl+Alt+N (IntelliJ)
+
+    public class IntroduceAndInlineVariable
+    {
+        public void Calculate(int a, int b)
+        {
+            // 1. Introduce variable
+            //    Select "a+b" (Use expand selection Ctrl+Alt+Right (VS) Ctrl+W (IntelliJ))
+            //    Invoke Introduce Variable
+            //    Specify type, or 'var', and name
+            Console.WriteLine(a + b);
+        }
+
+        public void Calculate2(int a, int b)
+        {
+            // 2. Introduce variable from multiple usages
+            //    Select one of the "a+b" (Use expand selection Ctrl+Alt+Right (VS) Ctrl+W (IntelliJ))
+            //    Invoke Introduce Variable
+            //    ReSharper highlights all usages, prompts for one usage or all
+            //    Select all, they all get replaced
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + b);
+        }
+
+        public void Calculate3(int a, int b)
+        {
+            // 1. Inline variable
+            //    Select "value" (Use expand selection Ctrl+Alt+Right (VS) Ctrl+W (IntelliJ))
+            //    Invoke Inline Variable
+            //    Variable is removed, and usage is replaced with "a+b"
+            var value = a + b;
+            Console.WriteLine(value);
+        }
+    }
+}
--- 04-Refactoring/07-Inline.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Refactoring
-{
-    public class Inline
-    {
-         
-    }
-}
