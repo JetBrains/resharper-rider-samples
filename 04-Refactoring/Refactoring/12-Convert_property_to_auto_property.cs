27cfd1a72f2ad5e81e1f222e9060d4f60ebb0fd8
Matt Ellis <m.t.ellis@gmail.com>
2015-02-11 16:21:41 +0000

    Convert property to auto-property

---
+++ 04-Refactoring/Refactoring/12-Convert_property_to_auto_property.cs
@@ -0,0 +1,44 @@
+﻿namespace JetBrains.ReSharper.Koans.Refactoring
+{
+    // Convert Property to Auto Property
+    //
+    // Refactoring has no keyboard shortcut. Invoke via Refactor This menu
+    // Ctrl+Shift+R
+    //
+    // Also available as Quick Fix via Alt+Enter
+
+    public class ConvertPropertyToAutoProperty
+    {
+        private string backingStore;
+        private string backingStore2;
+        private string backingStore3;
+
+        // 1. Convert to Auto Property with refactoring
+        //    Place text caret on property definition
+        //    Invoke Refactor This menu → Convert Property to Auto-Property
+        public string PropertyWithBackingStore
+        {
+            get { return backingStore; }
+            set { backingStore = value; }
+        }
+
+        // 2. Convert to Auto Property with Alt+Enter
+        //    Note shown as suggestion
+        //    Place text caret on property definition
+        //    Alt+Enter and select Convert to auto-property
+        public string PropertyWithBackingStore2
+        {
+            get { return backingStore2; }
+            set { backingStore2 = value; }
+        }
+
+        // 2. Convert to read-only Auto Property with Alt+Enter
+        //    Note shown as hint
+        //    Place text caret on property definition
+        //    Alt+Enter and select Convert to auto-property
+        public string PropertyWithBackingStore3
+        {
+            get { return backingStore3; }
+        }
+    }
+}
