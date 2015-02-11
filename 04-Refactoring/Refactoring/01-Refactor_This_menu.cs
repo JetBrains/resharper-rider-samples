de6110070d063333d3e60a1d9874203d84713b54
Matt Ellis <m.t.ellis@gmail.com>
2015-02-11 21:45:47 +0000

    Refactor This menu

--- 04-Refactoring/Refactoring/01-Refactor_This_menu.cs
+++ 04-Refactoring/Refactoring/01-Refactor_This_menu.cs
@@ -1,13 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Refactoring
+namespace JetBrains.ReSharper.Koans.Refactoring
 {
+    // Refactor This menu
+    //
+    // Display a contextual menu of refactorings available at the current location
+    //
+    // Ctrl+Shift+R
 
-    public class Class1
+    // 1. Refactor This on type definition, method, property, field, parameter, variable, etc.
+    //    Note different options on each code element
+    //    Various refactorings shown in more detail in rest of section
+    public class Class1 // <- Refactor This on class name
     {
+        // Refactor This on field
+        public readonly int field;
+
+        // Refactor This on property
+        public string Property { get; set; }
+
+        // Refactor This on method, parameters
+        public void Method(string parameter1, int parameter2)
+        {
+            // Refactor This on variable
+            var variable = parameter1;
+            Console.WriteLine(variable);
+        }
     }
 }
