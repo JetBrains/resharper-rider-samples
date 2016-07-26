0535c5dc18bf3f99f9566ffb3250da6194e32527
Vic Peters <vicp@iqmetrix.com>
2016-07-26 15:23:29 -0500

    Applied tweak to make Ctrl+- work

---
+++ 01-Navigation/3-Navigate_to_somewhere_youve_already_been/NavigateTo.cs
@@ -0,0 +1,10 @@
+﻿namespace JetBrains.ReSharper.Koans.Navigation
+{
+    public class NavigateTo
+    {
+        public void Destination()
+        {
+            // Now navigate back, using Visual Studio's Ctrl+- (minus)
+        }
+    }
+}
