aae1bde084f23c5d4793c54643a3911dac07e888
sun.flat.yamada <sun.flat.yamada@hotmail.com>
2021-05-03 18:49:03 +0900

    Translate to Japanese

--- localized/ja/01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.3-Recently_changed_files.cs
+++ localized/ja/01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.3-Recently_changed_files.cs
@@ -2,11 +2,16 @@
 
 namespace JetBrains.ReSharper.Koans.Navigation
 {
-    // Recently Changed Files
+    // 最近編集したファイルのリストに移動する方法を試してみます。
     //
-    // Shows a popup menu of recent edits. Shows code blocks, not files
+    //TODO: ReSharperで最近の編集表示機能 (View Recent Edits) が動作しない (ReSharper 2021.1 on VisualStudio2019)
+    //TODO: Riderで最近の編集表示機能 (View Recent Edits) はコードブロックでなく、ファイルリストで表示される (Rider 2021.2)
     //
-    // <shortcut id="Recently Changed Files">Ctrl+Shift+, (VS)</shortcut>
+    // 最近の編集内容をポップアップメニューで表示出来ます。
+    // (ReSharper) ファイルではなくコードブロックが表示出来ます。
+    //
+    // <shortcut id="Recently Changed Files">Ctrl+Shift+,    (ReSharper VisualStudio Keymap)</shortcut>
+    // <shortcut id="Recently Changed Files">Ctrl+E (Toggle) (Rider Default IntelliJ Keymap)</shortcut>
     // 
 
     public class RecentEdits
@@ -22,17 +27,22 @@ namespace JetBrains.ReSharper.Koans.Navigation
 
         private string GetName()
         {
-            // 1. Implement GetName by returning firstName
+            // 1. firstName を返す GetName を実装してください。
+            //    コメントを外して Exception の行はコメントアウトしてください。
+            // return firstName; 
             throw new NotImplementedException();
         }
 
         public void Dump()
         {
-            // 2. Implement by uncommenting
+            // 2. コメントを外して実装してください。
             // Console.WriteLine("Name: {0}", GetName());
 
-            // 3. Return to GetName using Recent Edits and change to be firstName + lastName
-            // 4. Return back here and uncomment the following
+            // 3. GetName に ”Recent Edits" を使って戻り、 firstName + lastName に実装を変更します。
+
+            // 4. ここに戻って以下のコメントを外してください。
+            //    (この実装に大きな意味はありません。移動の操作感を体験してもらう内容です。)
+            //TODO: Riderではこの演習は機能しない。コードブロック表示ではなくファイルリストで、自ファイルは表示されない。
             // Console.WriteLine("Done");
         }
     }
--- localized/ja/01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.4-Bookmarks.cs
+++ localized/ja/01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.4-Bookmarks.cs
@@ -1,30 +1,47 @@
 ﻿namespace JetBrains.ReSharper.Koans.Navigation
 {
-    // Bookmarks
+    // ブックマーク機能を試してみます。
     //
-    // TODO: evaluate the shortcuts here
+    // TODO: ReSharperでブックマーク機能が動作しない (ReSharper 2021.1 on VisualStudio2019)
     // 
-    // Set, unset and navigate to bookmarks
+    // ブックマークを付けたり、外したり、ブックマーク先に移動したりします。
     //
-    // <shortcut id="Toggle Bookmark with Mnemonic">Ctrl+F11</shortcut> to toggle a numbered bookmark
-    // <shortcut id="">Ctrl+[0-9]</shortcut> to navigate to a numbered bookmark
-    // <shortcut id="Show Bookmarks">Ctrl+`</shortcut> to show a popup menu of bookmarks
+    // 簡易見出し付き (with ニーモニック) のブックマークの付け外し
+    // <shortcut id="Toggle Bookmark with Mnemonic">Ctrl+F11 (Common)</shortcut>
     //
-    // 1. Navigate to MethodOne (using Go To File Member!) and set bookmark one
-    //    Ctrl+Shift+1 - not the bookmark icon in the editor gutter
+    // 簡易見出し付きブックマークへの移動
+    // <shortcut id="">Ctrl+[0-9] (Common)</shortcut>
+    //
+    // ブックマークリストのポップアップ表示
+    // <shortcut id="Show Bookmarks">Ctrl+`    (ReSharper VisualStudio Keymap)</shortcut>
+    // <shortcut id="Show Bookmarks">Shift+F11 (Rider Default IntelliJ Keymap)</shortcut>
+    //
+    // 簡易見出し付きブックマークの直接作成 (上書き)
+    // <shortcut id="Toggle Bookmark with X">Ctrl+Shift+[0-9] (Common)</shortcut>
+    //
+    // 1. 下に定義のある "MethodOne" に移動し、簡易見出し付きブックマーク "1" を設定します。
+    //    メソッドの行にキャレットを置き、ショートカットキーを押します。
+    //    <shortcut id="Toggle Bookmark with Mnemonic 1">Ctrl+Shift+1</shortcut> 
+    //    エディター行番号付近の余白のブックマークアイコンではなく、ショートカットキーで操作しましょう。
     // 
-    // 2. Navigate to MethodTwo and MethodThree and set bookmarks two and three
+    // 2. 同様に、 "MethodTwo" と "MethodThree" に 2 と 3 のブックマークを設定します。
     //
-    // 3. Navigate between bookmark one, two and three
+    // 3. 設定した３つのブックマークを移動してみます。
+    //    以下のショートカットで移動してみましょう。
     //    Ctrl+1, Ctrl+2, Ctrl+3
     //
-    // 4. Remove or reposition any of the bookmarks
+    // 4. ブックマークを削除したり、別の場所に移動したりしてみます。
+    //    ブックマークのある行で同じ番号のショートカットを押すと、トグルで削除されます。
+    //    他の行でショートカットを押すと、押したショートカットの番号は移動します。
     //    Ctrl+Shift+1, Ctrl+Shift+2, etc
     //
-    // 5. Display the popup menu to navigate between bookmarks
+    // 5. ブックマーク一覧を表示して移動してみましょう。
+    //    <shortcut id="Show Bookmarks">Ctrl+`    (ReSharper VisualStudio Keymap)</shortcut>
+    //    <shortcut id="Show Bookmarks">Shift+F11 (Rider Default IntelliJ Keymap)</shortcut>
     //
-    // 6. From the popup menu, set an anonymous bookmark. Navigate back to the
-    //    anonymous bookmark from the menu
+    // 6. ポップアップメニューから匿名のブックマークを設定します。
+    //    Shift+F11 でポップアップメニューを開き、コードを選択して F11 を押して匿名のブックマークを設定します。
+    //    ブックマークのリストを選択すると該当ブックマークの箇所に移動できます。
     public class Bookmarks
     {
         public void MethodOne()
