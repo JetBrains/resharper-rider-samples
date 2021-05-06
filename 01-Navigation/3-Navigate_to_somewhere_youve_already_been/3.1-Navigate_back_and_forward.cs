f5eee3b6f0d0ddb08e407432688e3a12f51aab4d
Youhei Yamada <51235130+sun-flat-yamada@users.noreply.github.com>
2021-05-07 01:03:44 +0900

    Add forward operation to workshop on "3.1-Navigate_back_and_forward.cs" (#33)
    * Translate to Japanese
    
    * Update keymap table
    
    * Add progress comment
    
    * Translate to Japanese
    
    * Translate to Japanese
    
    * Fix recent edits workshop
    
    * Rename to adequately describes the content
    
    * Del resolved TODO
    
    * Rename to adequately describes the content
    
    * Reflect improvements in the Japanese version
    
    * #31-include-both-back-and-forward-operations
    
    * #31-include-both-back-and-forward-operations

--- 01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.1-Navigate_back_and_forward.cs
+++ 01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.1-Navigate_back_and_forward.cs
@@ -1,10 +1,16 @@
 ﻿namespace JetBrains.ReSharper.Koans.Navigation
 {
-    // Navigate Back
+    // Navigate Back and Forward
     //
     // Returns the caret to the last navigation point
     //
-    // <shortcut id="Back">Ctrl+- (minus)</shortcut>
+    // Back
+    // <shortcut id="Navigate back">Ctrl+- (minus) (ReSharper VisualStudio Keymap)</shortcut>
+    // <shortcut id="Navigate back">Ctrl+Alt+Left  (Rider Default IntelliJ Keymap)</shortcut>
+    //
+    // Forward
+    // <shortcut id="Navigate forward">Ctrl+Shift+- (minus) (ReSharper VisualStudio Keymap)</shortcut>
+    // <shortcut id="Navigate forward">Ctrl+Alt+Right       (Rider Default IntelliJ Keymap)</shortcut>
     //
     public class NavigateBack
     {
@@ -12,7 +18,19 @@
         {
             var navigateTo = new NavigateTo();
 
-            // 1. Navigate to NavigateTo.Destination by <shortcut id="Go to Declaration or Usages">F12 or CTRL + Click</shortcut>
+            // 1. Navigate to "Destination" by "Go to Declaration"
+            //    Do a "Back" operation on the destination.
+            //
+            // Do you remember the move to declaration operation?
+            // If you don't remember, here they are :
+            // <shortcut id="Go to Declaration or Usages">F12    or CTRL + Click (ReSharper VisualStudio Keymap)</shortcut>
+            // <shortcut id="Go to Declaration or Usages">Ctrl+B or CTRL + Click (Rider Default IntelliJ Keymap)</shortcut>
+            //
+            // 2. When you come back to this location,
+            //    do a "Forward" operation.
+            //    You should be able to see again the declaration of "Destination"
+            //    that we referred to earlier.
+            //
             navigateTo.Destination();
         }
     }
--- localized/ja/01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.1-Navigate_back_and_forward.cs
+++ localized/ja/01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.1-Navigate_back_and_forward.cs
@@ -1,13 +1,18 @@
 ﻿namespace JetBrains.ReSharper.Koans.Navigation
 {
-    // 戻る操作をしてみます。
+    // 前後の操作箇所の間を移動する操作をしてみます。
     //
-    // カーソル (キャレット) を最後に操作していた場所に戻します。
+    // カーソル (キャレット) を操作していた前後の場所に移動します。
     //
+    // 戻る
     // <shortcut id="Navigate back">Ctrl+- (minus) (ReSharper VisualStudio Keymap)</shortcut>
     // <shortcut id="Navigate back">Ctrl+Alt+Left  (Rider Default IntelliJ Keymap)</shortcut>
     //
-    public class NavigateBack
+    // 進む
+    // <shortcut id="Navigate forward">Ctrl+Shift+- (minus) (ReSharper VisualStudio Keymap)</shortcut>
+    // <shortcut id="Navigate forward">Ctrl+Alt+Right       (Rider Default IntelliJ Keymap)</shortcut>
+    //
+    public class NavigateBackAndForward
     {
         public void StartingPoint()
         {
@@ -15,11 +20,13 @@
 
             // 1. "Destination" に対して、宣言への移動操作を行います。
             //      移動した先で戻る操作をしてみてください。
-            //      この場所に戻ってきたら、今度は進む操作をしてみましょう。
             //
             // 宣言への移動操作は憶えていますか？忘れた人は以下です。
             // <shortcut id="Go to Declaration or Usages">F12    or CTRL + Click (ReSharper VisualStudio Keymap)</shortcut>
             // <shortcut id="Go to Declaration or Usages">Ctrl+B or CTRL + Click (Rider Default IntelliJ Keymap)</shortcut>
+            //
+            // 2. この場所に戻ってきたら、今度は進む操作をしてみましょう。
+            //    先ほど参照した "Destination" の宣言をもう一度確認することが出来るはずです。
             //
             navigateTo.Destination();
         }
